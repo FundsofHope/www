4973e4ced5b6a651f8a89e3b4b5b097400f37b43
Aditya <addiittya2006@gmail.com>
2015-10-18 18:14:13 +0530

    Correcting cred

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	"database": "mongodb://root:fundsofhope@ds047722.mongolab.com:47722/userstore",
+	"database": "mongodb://root:funds0fhope@ds047722.mongolab.com:47722/userstore",
 	"port": process.env.PORT || 3000,
 	"secretKey": "password"
 
