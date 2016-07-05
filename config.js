966ebabadf3c60f907dbd0a7b6f98228984b5841
Aditya <addiittya2006@gmail.com>
2016-07-05 17:54:38 +0530

    add polymer element

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	"local_db": "mongodb:///Applications/MEAN/mongodb/tmp/mongodb-27017.sock/minor",
+	"local_db": "mongodb:///Users/addiittya/Developer/JS/mongodb/tmp/mongodb-27017.sock/minor",
 	"remote_db": "mongodb://root:funds0fhope@ds047722.mongolab.com:47722/userstore",
 	"port": process.env.PORT || 3000,
 	"secretKey": "password"
